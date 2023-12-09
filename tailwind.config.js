8a4ae0b21c261ba8eb3c66b3df8551102a9fa0fc
Mathis Boulais <https://github.com/Asukyy>
2023-12-09 12:37:57 +0100

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "assets/**/*.{vue,js,ts,jsx,tsx}",
+    "templates/**/*.{twig,html}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
